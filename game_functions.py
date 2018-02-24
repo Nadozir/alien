c7c88d42cc980d49f8a4d98059c4159e8bb5dcff
alucard <azovl@ukr.net>
2018-02-24 11:11:42 +0200

    Added bullet class.

--- game_functions.py
+++ game_functions.py
@@ -8,10 +8,10 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
-    elif event.key == pygame.K_SPACE:                
-        new_bullet = Bullet(ai_settings, screen, ship)         
-        bullets.add(new_bullet) 
-
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:         
+        sys.exit() 
 
 def check_keyup_events(event, ship):     
     if event.key == pygame.K_RIGHT:
@@ -32,6 +32,18 @@ def update_screen(ai_settings, screen, ship, bullets):
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():         
         bullet.draw_bullet() 
+
+def update_bullets(bullets):      
+    bullets.update() 
+    for bullet in bullets.copy(): 
+        if bullet.rect.bottom <= 0: 
+            bullets.remove(bullet) 
+
+def fire_bullet(ai_settings, screen, ship, bullets):     
+    if len(bullets) < ai_settings.bullets_allowed: 
+        new_bullet = Bullet(ai_settings, screen, ship) 
+        bullets.add(new_bullet) 
+
     ship.blitme()
 
     pygame.display.flip()
