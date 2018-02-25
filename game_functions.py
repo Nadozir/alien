bae3e8ef543b5562af1e4cb37f6ba1f0b5f202e2
alucard <azovl@ukr.net>
2018-02-25 12:35:03 +0200

    Added alien class.

--- game_functions.py
+++ game_functions.py
@@ -1,7 +1,8 @@
 import sys
 
 import pygame
-from bullet import Bullet 
+from bullet import Bullet
+from alien import Alien  
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
@@ -28,10 +29,12 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     screen.fill(ai_settings.bg_color)
     for bullet in bullets.sprites():         
-        bullet.draw_bullet() 
+        bullet.draw_bullet()
+        ship.blitme()
+        aliens.draw(screen)
 
 def update_bullets(bullets):      
     bullets.update() 
@@ -44,6 +47,16 @@ def fire_bullet(ai_settings, screen, ship, bullets):
         new_bullet = Bullet(ai_settings, screen, ship) 
         bullets.add(new_bullet) 
 
-    ship.blitme()
+def create_fleet(ai_settings, screen, aliens):     
+    alien = Alien(ai_settings, screen) 
+    alien_width = alien.rect.width 
+    available_space_x = ai_settings.screen_width - 2 * alien_width 
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+          
+    for alien_number in range(number_aliens_x):         
+        alien = Alien(ai_settings, screen)         
+        alien.x = alien_width + 2 * alien_width * alien_number         
+        alien.rect.x = alien.x         
+        aliens.add(alien) 
 
     pygame.display.flip()
